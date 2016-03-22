f743343981fc1b0a38a0d2e6cb98c3b894df066b
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-03-22 17:28:15 +1100

    Turn off forcing SSL

--- app/app.rb
+++ app/app.rb
@@ -6,7 +6,7 @@ class App < Roda
   plugin :default_headers,
          'Content-Type'              => 'text/html',
          'Content-Security-Policy'   => "default-src 'self'",
-         'Strict-Transport-Security' => 'max-age=16070400;',
+         # 'Strict-Transport-Security' => 'max-age=16070400;',
          'X-Frame-Options'           => 'deny',
          'X-Content-Type-Options'    => 'nosniff',
          'X-XSS-Protection'          => '1; mode=block'
